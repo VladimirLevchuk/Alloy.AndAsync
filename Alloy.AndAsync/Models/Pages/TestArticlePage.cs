8dc62eaa45e76730e5e67559f9e50eea1ec01858
Vladimir Levchuk <vladimir.levchuk@creuna.no>
2015-10-01 11:46:24 +0300

    added test article page with async controller

---
+++ Alloy.AndAsync/Models/Pages/TestArticlePage.cs
@@ -0,0 +1,11 @@
+namespace Alloy.AndAsync.Models.Pages
+{
+    [SiteContentType(
+        GroupName = Global.GroupNames.News,
+        GUID = "{CE6A78E9-2729-4B93-A8EE-D088BA344100}")]
+    [SiteImageUrl(Global.StaticGraphicsFolderPath + "page-type-thumbnail-article.png")]
+    public class TestArticlePage : ArticlePage
+    {
+
+    }
+}
