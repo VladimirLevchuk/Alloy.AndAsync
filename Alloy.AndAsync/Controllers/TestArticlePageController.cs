e4b08a4474a6760eabf04c640539eee86d90c5de
Vladimir Levchuk <vladimir.levchuk@creuna.no>
2015-10-02 10:26:27 +0300

    Playing a bit with sync partial which calls HttpClient (no deadlock now)

--- Alloy.AndAsync/Controllers/TestArticlePageController.cs
+++ Alloy.AndAsync/Controllers/TestArticlePageController.cs
@@ -3,7 +3,9 @@ using System.Threading.Tasks;
 using System.Web.Mvc;
 using Alloy.AndAsync.Models.Pages;
 using Alloy.AndAsync.Models.ViewModels;
+using EPiServer.ServiceLocation;
 using EPiServer.Web;
+using EPiServer.Web.Routing;
 using Microsoft.Ajax.Utilities;
 
 namespace Alloy.AndAsync.Controllers
@@ -14,19 +16,38 @@ namespace Alloy.AndAsync.Controllers
         {
             var result = new TestArticlePageViewModel(currentPage)
             {
-                SomeExternalData = await GetExternalData(),
-                SiteStartPageId = SiteDefinition.Current.IfNotNull(x => x.StartPage.ID)
+                SomeExternalData = await GetExternalDataAsync(),
+                SiteStartPageId = SiteDefinition.Current.IfNotNull(x => x.StartPage.ID),
             };
 
             return View(result);
         }
 
-        public async Task<string> GetExternalData()
+        public async Task<string> GetExternalDataAsync()
         {
             using (var client = new HttpClient())
             {
                 return await client.GetStringAsync("http://google.com");
             }
+        }
+
+        public string GetExternalData()
+        {
+            using (var client = new HttpClient())
+            {
+                return client.GetStringAsync("http://google.com").Result;
+            }
+        }
+
+        public ViewResult Action(TestArticlePage currentPage)
+        {
+            var result = new TestArticlePageViewModel(currentPage)
+            {
+                SomeExternalData = GetExternalData(),
+                SiteStartPageId = SiteDefinition.Current.IfNotNull(x => x.StartPage.ID)
+            };
+
+            return View(result);
         }
     }
 }
